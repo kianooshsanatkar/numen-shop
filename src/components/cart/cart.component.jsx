ec59c79b34179dd2065b57c60496700e727ac190
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-03 19:48:59 +0330

    a bit cleaning

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
-import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
+import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
 
-import { mapDispatchToProps, mapStateToProps } from "../../redux/cart/cart-reducer";
+import {
+  mapDispatchToProps,
+  mapStateToProps,
+} from "../../redux/cart/cart-reducer";
 import CartItem from "./cart-item.component";
 import "./cart.style.css";
 
 class Cart extends Component {
   render() {
     return (
-      <aside className="cart-container" style={this.props.cartVisibility?{right:0}:null}>
-          
-        <div className='close-cart-button'>
-            <CloseOutlinedIcon 
-                onClick={() => {this.props.setCartVisibility()}}
-                fontSize='large' 
-                ></CloseOutlinedIcon>
+      <aside
+        className="cart-container"
+        style={this.props.cartVisibility ? { right: 0 } : null}
+      >
+        <div className="close-cart-button">
+          <CloseOutlinedIcon
+            onClick={() => {
+              this.props.setCartVisibility();
+            }}
+            fontSize="large"
+          ></CloseOutlinedIcon>
         </div>
-        <div className='cart-logo'>
-            <ShoppingCartOutlinedIcon color="disabled" fontSize='large'></ShoppingCartOutlinedIcon>
+        <div className="cart-logo">
+          <ShoppingCartOutlinedIcon
+            color="disabled"
+            fontSize="large"
+          ></ShoppingCartOutlinedIcon>
         </div>
-        <div className='border-line'></div>
+        <div className="border-line"></div>
 
         <ul className="cart-items-container">
           {this.props.cartItems
@@ -36,4 +46,4 @@ class Cart extends Component {
     );
   }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);

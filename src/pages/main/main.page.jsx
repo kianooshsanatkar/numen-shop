539a7674eb7801fe33f4b890cf3a57da1b2e0a06
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-09 05:34:07 +0330

    improve main page

--- src/pages/main/main.page.jsx
+++ src/pages/main/main.page.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Container, Grid, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import "./main.style.css";
 import BgImage from '../../resource/images/IMG_7129.JPG';
+import ProductSlider from "../../components/product-slider/product-slider.component";
+import HeightSpace from '../../components/space/space.component';
 
 class Main extends Component{
     render(){
@@ -12,10 +16,20 @@ class Main extends Component{
                         <img src={BgImage} alt='Numen Flame' className='main-bg-cover' />
                     </Grid>
                     <Grid item sm={6} xs={12} className='main-title'>
-                        <Typography variant='h2'>نومن</Typography>
-                        <Typography variant='h6'>محصولات  دست ساز برای مراقبت و زیبایی پوست و مو</Typography>
+                        <Typography variant='h1' className="yekan-text">نومن</Typography>
+                        <Typography variant='h4' className="yekan-text">محصولات  دست ساز</Typography>
+                        <Typography variant='body1' className="yekan-text">برای مراقبت و زیبایی پوست</Typography>
                     </Grid>
                 </Grid>
+                <HeightSpace />
+                <Grid container>
+                    <Grid item xs={12}>
+                        <ProductSlider />
+                    </Grid>
+                </Grid>
+                <div className="scroll-down-button">
+                    <ExpandMoreIcon fontSize="large"></ExpandMoreIcon>
+                </div>
             </Container>
         </main>
     }

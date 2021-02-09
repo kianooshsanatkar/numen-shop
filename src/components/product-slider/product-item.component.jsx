dbebc2a6f8366020985aa6808893f690631c7a78
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-09 05:30:22 +0330

    Add and implement Product Slider (currently not gonna slide just show)

---
+++ src/components/product-slider/product-item.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+
+export default function ProductItem(props) {
+  let link_path = "http://127.0.0.1:5000/static/images/";
+  let suffix = "_small.jpg";
+  let images = props.product.images ? props.product.images.split(",") : null;
+  return (
+    <Grid item xs={6} sm={3}>
+      <div className="product-item">
+        {images ? (
+          <img src={link_path + images[0] + suffix} alt={props.product.title} />
+        ) : null}
+        <div className="product-item-title">
+          <Typography variant="h4" className="yekan-text">
+            {props.product.title}
+          </Typography>
+        </div>
+      </div>
+    </Grid>
+  );
+}
---
+++ src/components/product-slider/product-slider.component.jsx
@@ -0,0 +1,32 @@
+import { Grid } from "@material-ui/core";
+import React, { Component } from "react";
+
+import ProductItem from "./product-item.component";
+import './product-slider.style.css';
+
+class ProductSlider extends Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:5000/api/label/products/1")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ products: data });
+      });
+  }
+
+  render() {
+      console.log(this.state.products)
+    return (
+      <Grid container spacing={6}>
+        {this.state.products.map((product) => (
+          <ProductItem key={product.uid} product={product} />
+        ))}
+      </Grid>
+    );
+  }
+}
+
+export default ProductSlider;

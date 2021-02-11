de3a592d9b2ea1386ab0780cf01f0de405d9454f
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-11 22:09:55 +0330

    make product clickable and link to product detail page

--- src/components/product/product-item.component.jsx
+++ src/components/product/product-item.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
+import NoImage from "../../resource/images/no-image-available.jpg";
+import { Link } from "react-router-dom";
 
 export default function ProductItem(props) {
   let link_path = "http://127.0.0.1:5000/static/images/";
@@ -9,9 +11,14 @@ export default function ProductItem(props) {
   return (
     <Grid item xs={6} sm={3}>
       <div className="product-item">
-        {images ? (
-          <img src={link_path + images[0] + suffix} alt={props.product.title} />
-        ) : null}
+        {
+          <Link to={"/product/"+props.product.uid}>
+            <img
+              src={!images ? NoImage : link_path + images[0] + suffix}
+              alt={props.product.title}
+            />
+          </Link>
+        }
         <div className="product-item-title">
           <Typography variant="h5" className="yekan-text">
             {props.product.title}

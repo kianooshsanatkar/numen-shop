e1c57a0150ab243a574b77d1c88d55bbd18530aa
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-12 05:35:27 +0330

    cart component (parent of cart-item)

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
+import Grid from "@material-ui/core/Grid";
 
 import {
   mapDispatchToProps,
@@ -11,6 +12,13 @@ import CartItem from "./cart-item.component";
 import "./cart.style.css";
 
 class Cart extends Component {
+  calculateTotalPrice = () => {
+    if (!this.props.cartItems) return null;
+    return this.props.cartItems.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
+  };
   render() {
     return (
       <aside
@@ -34,14 +42,25 @@ class Cart extends Component {
         <div className="border-line"></div>
 
         <ul className="cart-items-container">
-          {this.props.cartItems
-            ? this.props.cartItems.forEach((item) => {
-                <li>
-                  <CartItem key={item.uid} prop={item} />
-                </li>;
-              })
+          {Array.isArray(this.props.cartItems)
+            ? this.props.cartItems.map((item) => (
+                <li key={item.uid}>
+                  <CartItem product={item} />
+                </li>
+              ))
             : null}
         </ul>
+        {Array.isArray(this.props.cartItems) &&
+        this.props.cartItems.length > 0 ? (
+          <Grid className="total-price-container" container justify="center">
+            <Grid item xs={8}>
+              <h2>{this.calculateTotalPrice()}</h2>
+            </Grid>
+            <Grid item xs={4}>
+              <h3>جمع کل</h3>
+            </Grid>
+          </Grid>
+        ) : null}
       </aside>
     );
   }

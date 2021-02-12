fb1531db1752b02f61b8bfbf6d7c12092bbfca1e
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-13 01:12:47 +0330

    implement localStorage persist for cart items

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
+import store, {persistor} from './redux/store';
 
-import store from './redux/store';
+import './index.css';
+
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
       <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );

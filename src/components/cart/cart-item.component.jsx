696fcef3dd2bd2bd8f406c6e9a6b97cad74b1a94
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-12 05:34:54 +0330

    design and implement cart-item funxtionality

--- src/components/cart/cart-item.component.jsx
+++ src/components/cart/cart-item.component.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Grid } from "@material-ui/core";
+import React from "react";
 
-export default function CartItem(){
-    return <div>
-        
+import NoImage from "../../resource/images/no-image-available.jpg";
+
+function getImagePath(fileName, size = "thumbnail") {
+  if (fileName === null || fileName === undefined || fileName === "")
+    return NoImage;
+  let link_path = "http://127.0.0.1:5000/static/images/";
+  return link_path + fileName + "_" + size + ".jpg";
+}
+export default function CartItem(props) {
+  return (
+    <div className="cart-item" style={{ borderBottom: "solid 1px black" }}>
+      <Grid container justify="center">
+        <Grid item xs={4}>
+          <img
+            style={{ width: "70px" }}
+            src={getImagePath(props.product.image)}
+            alt={props.product.title}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <h3>{props.product.title}</h3>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={4}>
+          <h3>{props.product.price * props.product.quantity}</h3>
+        </Grid>
+        <Grid item xs={8}>
+          <h3>{props.product.quantity}</h3>
+        </Grid>
+      </Grid>
     </div>
+  );
 }

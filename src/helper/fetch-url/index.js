7849c20fa6f29df7dcb9bb972ba920dbd7dec044
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-22 04:22:52 +0330

    a url holder for handeling dependency to urls

---
+++ src/helper/fetch-url/index.js
@@ -0,0 +1,18 @@
+// Todo: set { prefix = '' } for production
+const prefix = "http://localhost";
+
+const URLs = {
+    Login: prefix + "/auth/login",
+    Logout: prefix + "/auth/logout",
+    Access: prefix + "/auth/token/access/",
+    Refresh: prefix + "/auth/token/refresh/",
+    Fresh: prefix + "/auth/fresh/",
+    User: prefix + "/api/user/",
+    Label: prefix + "/api/label/",
+    Labels: prefix + "/api/labels/",
+    Product: prefix + "/api/product/",
+    Products: prefix + "/api/label/products/",
+    Cart: prefix + "/api/cart/",
+}
+
+export default URLs;

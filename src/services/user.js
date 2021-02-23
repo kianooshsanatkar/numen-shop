b3fddc224e7cdcfc688075eb5e2b2d4414d06a5e
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-23 23:24:18 +0330

    add user service functionality

--- src/services/user.js
+++ src/services/user.js
@@ -1,8 +1,24 @@
-export function createUser(){
+import URLs from '../helper/fetch-url';
+import { getHeaderAccAuth } from './auth';
+
+export function createUser() {
 
 }
 
-export function updateUser(){
-
+export async function updateUser(user) {
+    const response = await fetch(URLs.User,{
+        method:"PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem('jwt_access')}`
+        },
+        body:JSON.stringify(user)
+    })
+    return response.ok
 }
 
+export async function getUser() {
+    return await(await fetch(URLs.User, {
+        headers: getHeaderAccAuth()
+    })).json();
+}

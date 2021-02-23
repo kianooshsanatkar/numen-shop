1f23ee16356f4430d868d76deb8378ea6c604eea
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-23 16:17:29 +0330

    add discountCalculator to helper/calculator

--- src/helper/calculator.js
+++ src/helper/calculator.js
@@ -7,7 +7,7 @@ export const calculateTotalPrice = (cartItems) => {
 };
 
 export const FormatPrice = (price) => {
-    return FormatNumber(price/10);
+    return FormatNumber(price / 10);
 }
 
 export const FormatNumber = (number) => {
@@ -16,7 +16,19 @@ export const FormatNumber = (number) => {
     let arr = [];
     do {
         arr.push(num.slice(-3));
-        num=num.slice(0, -3);
+        num = num.slice(0, -3);
     } while (num);
     return arr.reverse().join(',')
+}
+
+export const calculateTotalDiscount = (totalPrice, discount) => {
+    let discount_amount = 0;
+    if (discount) {
+        if (totalPrice > discount.min_price) {
+            discount_amount = totalPrice * discount.percentage / 100;
+            if (discount.max_discount && discount_amount > discount.max_discount)
+                discount_amount = discount.max_discount
+        }
+    }
+    return discount_amount;
 }

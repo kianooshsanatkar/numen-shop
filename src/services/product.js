7b73c9969ab9dbab7e0870b40bb7595ee71d8d35
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-23 15:53:35 +0330

    add service to productService

--- src/services/product.js
+++ src/services/product.js
@@ -1,4 +1,5 @@
 import URLs from '../helper/fetch-url';
+import {getHeaderAccAuth} from './auth';
 
 export async function getProduct(productId) {
     const result = await (await fetch(URLs.Product + productId)).json()
@@ -8,4 +9,14 @@ export async function getProduct(productId) {
 export async function getProducts(labelId) {
     const result = await (await fetch(URLs.Products + labelId)).json();
     return result;
+}
+
+export async function getDiscount(couponCode) {
+    const response = await fetch(URLs.Discount + couponCode,{
+        headers:getHeaderAccAuth()
+    })
+    if (response.ok)
+        return await response.json()
+    else
+        return null;
 }

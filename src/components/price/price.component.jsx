f43ad9eb95ab115d0d2732dc4e2254c34b696b61
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-23 15:54:01 +0330

    update price component

--- src/components/price/price.component.jsx
+++ src/components/price/price.component.jsx
@@ -1,13 +1,21 @@
-import { Typography } from '@material-ui/core'
-import React from 'react';
-import {FormatPrice} from '../../helper/calculator';
-
+import { Typography } from "@material-ui/core";
+import React from "react";
+import { FormatPrice } from "../../helper/calculator";
 
 export default function Price(props) {
-    return(
-        <div>
-            <Typography variant="h6" color="textSecondary" component="span" style={{fontWeight:900, marginRight:'.2em'}}>T</Typography>
-            <Typography variant="h6" component="span"noWrap>{FormatPrice(props.price)}</Typography>
-        </div>
-    )
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color={props.color ? props.color : "textSecondary"}
+        component="span"
+        style={{ fontWeight: 900, marginRight: ".2em" }}
+      >
+        T
+      </Typography>
+      <Typography variant="h6" component="span" color={props.color?props.color:"textPrimary"} noWrap>
+        {FormatPrice(props.price)}
+      </Typography>
+    </div>
+  );
 }

da3005f1b4754912e9cf77a5ffa202ae3ea425b1
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-25 18:53:44 +0330

    Add new url

--- src/helper/fetch-url.js
+++ src/helper/fetch-url.js
@@ -5,15 +5,15 @@ const URLs = {
     Login: prefix + "/auth/login",
     Logout: prefix + "/auth/logout",
     Access: prefix + "/auth/token/access/",
+    Fresh: prefix + "/auth/token/fresh/",
     Refresh: prefix + "/auth/token/refresh/",
-    Fresh: prefix + "/auth/fresh/",
     User: prefix + "/api/user/",
     Label: prefix + "/api/label/",
     Labels: prefix + "/api/labels/",
     Product: prefix + "/api/product/",
     Products: prefix + "/api/label/products/",
     Cart: prefix + "/api/cart/",
-    Discount: prefix + '/api/discount/'
+    Discount: prefix + '/api/discount/',
 }
 
 export default URLs;

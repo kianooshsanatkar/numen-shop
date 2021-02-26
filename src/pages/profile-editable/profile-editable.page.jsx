5f0280b65da084083b3ee2fcddfd51c0d6aa02e9
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-02-26 19:32:08 +0330

    add update address to Update Profile

--- src/pages/profile-editable/profile-editable.page.jsx
+++ src/pages/profile-editable/profile-editable.page.jsx
@@ -10,16 +10,56 @@ import {
   Grid,
   Button,
 } from "@material-ui/core";
-import React, { useState } from "react";
-import { updateUser } from "../../services/user";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { updateUser, getUser } from "../../services/user";
+import { getAddress, updateAddress } from "../../services/address";
 
 export default function EditableProfile() {
   const history = useHistory();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+  });
+  const [address, setAddress] = useState({
+    city: "",
+    zip_code: "",
+    postal_address: "",
+  });
+  const [fetch, setFetch] = useState(false);
+
+  const userInStorage = useSelector((state) => state.user);
+  console.log(userInStorage);
+
+  useEffect(() => {
+    if (!fetch) {
+      setFetch(true);
+      getUser().then((u) => {
+        if (u)
+          setUser({
+            ...user,
+            ...u,
+          });
+      });
+      getAddress().then((ad) => {
+        if (ad)
+          setAddress({
+            ...address,
+            ...ad,
+          });
+      });
+    }
+  }, [user, address, fetch]);
 
   const userSetter = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const addressSetter = (e) => {
+    setAddress({ ...address, [e.target.name]: e.target.value });
   };
 
   return !user ? (
@@ -94,6 +134,57 @@ export default function EditableProfile() {
                       />
                     </TableCell>
                   </TableRow>
+                  <TableRow>
+                    <TableCell align="right">شهر :</TableCell>
+                    <TableCell align="left">
+                      <TextField
+                        error={address.city.length < 2}
+                        fullWidth
+                        value={address.city}
+                        onChange={addressSetter}
+                        required
+                        label="شهر"
+                        name="city"
+                        placeholder="تهران"
+                        variant="outlined"
+                      />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="right">کد پستی :</TableCell>
+                    <TableCell align="left">
+                      <TextField
+                        error={
+                          address.zip_code.length !== 0 &&
+                          address.zip_code.length !== 10
+                        }
+                        fullWidth
+                        value={address.zip_code}
+                        onChange={addressSetter}
+                        label="کد پستی"
+                        placeholder="1234567890"
+                        name="zip_code"
+                        variant="outlined"
+                      />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="right">آدرس :</TableCell>
+                    <TableCell align="left">
+                      <TextField
+                        error={address.postal_address.length < 10}
+                        multiline={true}
+                        placeholder="استان، شهر، محله، خیابان، کوچه، پلاک، واحد"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        label="آدرس"
+                        name="postal_address"
+                        value={address.postal_address}
+                        onChange={addressSetter}
+                      ></TextField>
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </TableContainer>
@@ -105,8 +196,14 @@ export default function EditableProfile() {
               size="large"
               fullWidth
               onClick={() => {
-                updateUser(user).then((sent) => {
-                  if (sent) history.push("/profile/");
+                updateUser(user).then((userSucceed) => {
+                  if (userSucceed) 
+                  updateAddress(address).then((addressSucceed)=>{
+                    if(addressSucceed)
+                    history.push("/profile/");
+                  }
+                  
+                  )
                 });
               }}
             >

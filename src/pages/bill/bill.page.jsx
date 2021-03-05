d2ad1c36131e5665fa6cf45c3f4b56492703e4ba
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-03-05 19:05:58 +0330

    update bill

--- src/pages/bill/bill.page.jsx
+++ src/pages/bill/bill.page.jsx
@@ -1,5 +1,41 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { Container, Grid, Paper } from "@material-ui/core";
+import PaymentIcon from "@material-ui/icons/Payment";
+import MarkunreadMailboxIcon from "@material-ui/icons/MarkunreadMailbox";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
+
+import { getInvoices } from "../../services";
+
+import InvoiceItem from './invoice-item.component';
+
 
 export default function BillPage() {
-  return <main></main>;
+  const [invoices, setInvoices] = useState(null);
+
+  useEffect(() => {
+    getInvoices().then((ins) => {
+      setInvoices(ins);
+    });
+  }, []);
+
+  return (
+    <main>
+      <Container fixed style={{ direction: "rtl" }}>
+        <div>
+          {invoices === null
+            ? null
+            : invoices.map((invoice) => {
+                return (
+                  <InvoiceItem
+                    key={invoice.uid}
+                    invoice={invoice}
+                  ></InvoiceItem>
+                );
+              })}
+        </div>
+      </Container>
+    </main>
+  );
 }

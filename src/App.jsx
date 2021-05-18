d8e73fbcc7212618b9fa6516894af555fb4b9a81
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-05-18 20:02:22 +0430

    refactor: rename + reorder imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import {ThemeProvider} from '@material-ui/core';
+import {createMuiTheme} from '@material-ui/core/styles';
 
-import Header from "./components/header/header.component";
-import Footer from "./components/footer/footer.component";
+import { saveUserStateAction } from "./redux/user.reducer";
+import { isLoggedIn } from "./services/auth";
+import "./App.css";
+
 import Main from "./pages/main/main.page";
 import Products from "./pages/products/products.page";
 import ProductPage from "./pages/product/product.page";
-import { saveUserStateAction } from "./redux/user.reducer";
-import "./App.css";
 import Profile from "./pages/profile/profile.page";
 import EditableProfile from "./pages/profile-editable/profile-editable.page";
 import CartPage from "./pages/cart";
-import { isLoggedIn } from "./services/auth";
-import {createMuiTheme} from '@material-ui/core/styles';
-import {ThemeProvider} from '@material-ui/core';
 import Register from "./pages/registration/registration.page";
 import BillPage from './pages/bill/bill.page';
 import InvoicePage from './pages/invoice/invoice.page';
+
+import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
+
 
 const theme = createMuiTheme({
   typography:{
@@ -30,8 +33,8 @@ export default function App() {
   const user = useSelector((state) => state.user.user);
   if (!user) {
     isLoggedIn().then((result) => {
-      const [logged, user] = result;
-      if (logged === true) dispatch(saveUserStateAction(user));
+      const [loggedIn, user] = result;
+      if (loggedIn === true) dispatch(saveUserStateAction(user));
     });
   }
 
